b4c36c2bf70db66288f90100e3e8c66aa23726c1
mayankresearcher <160275937+mayankreseracher@users.noreply.github.com>
2026-06-29 20:12:31 +0530

    Update main_test.go

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -5,8 +5,5 @@ import (
 )
 
 func TestPlaceholder(t *testing.T) {
-	// Placeholder test to ensure test suite runs
-	if true != true {
-		t.Fatal("Math is broken")
-	}
+	t.Log("Placeholder test")
 }
